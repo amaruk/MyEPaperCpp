ad02955020b030b398f31a71d4de4eabdd2102aa
amaruk <amaruk@163.com>
2017-07-07 21:44:35 +0200

    Create solution for C++ MyEpaper.

---
+++ MyEpaperCpp/MyEpaperCpp.cpp
@@ -0,0 +1,17 @@
+// MyEpaperCpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    cout << "Hello, C++ Epaper!" << endl;
+    return 0;
+}
+
